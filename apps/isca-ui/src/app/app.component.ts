82abeaa5654fd3ffaf211eb732d2827a36205592
elalexols <elalex_ols@hotmail.com>
2021-11-24 22:54:15 -0700

    sections

--- apps/isca-ui/src/app/app.component.ts
+++ apps/isca-ui/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import {Router, NavigationStart, NavigationEnd,NavigationCancel, NavigationError, Event
-} from '@angular/router';
+import {
+  Router, 
+  NavigationStart, 
+  NavigationEnd,
+  NavigationCancel, 
+  NavigationError, 
+  Event } from '@angular/router';
+// import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ISCA';
 
@@ -26,4 +33,10 @@ export class AppComponent {
 
     });
   }
+}
+
+export class CardMediaSizeExample {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }
