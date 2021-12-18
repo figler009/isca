c830915b67c40c5ecd156be9c1bd4e1016f83c6e
luis-figueroa <lfigueroa@powdevs.com>
2021-12-18 12:39:44 -0700

    removing amplify

--- apps/isca-ui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "iscaui": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
